4fdc5d0f952d8665e1c89f2190d5a5f4edc824dd
dsuccar <danielsuccar@icloud.com>
2020-04-20 15:21:21 -0400

    a

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
+
+function greet(time){
+
+    const hour = parseInt(time);
+    if (hour < 12) {
+        return 'Good Morning';
+    } else if (hour < 17) {
+        return 'Good Afternoon';
+    } else {
+        return 'Good Evening';
+    }
+    }
+
 
 /* End Given Code, don't edit above here...*/
 
